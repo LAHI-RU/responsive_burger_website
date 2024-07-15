e5e219e9d05fdd8ea46a4d16acf6cbdd52fbdb4d
Lahiru Dhananjaya <lahiiru.dananjaya@gmail.com>
2024-07-15 18:57:31 +0530

    update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,27 @@
 /*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById("nav-menu"),
+  navToggle = document.getElementById("nav-toggle"),
+  navClose = document.getElementById("nav-close");
 
+/* Menu show */
+if (navToggle) {
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.add("show-menu");
+  });
+}
 
+/* Menu hidden */
+if (navClose) {
+  navClose.addEventListener("click", () => {
+    navMenu.classList.remove("show-menu");
+  });
+}
 /*=============== REMOVE MENU MOBILE ===============*/
-
 
 /*=============== ADD SHADOW HEADER ===============*/
 
-
-/*=============== SHOW SCROLL UP ===============*/ 
-
+/*=============== SHOW SCROLL UP ===============*/
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
-
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
