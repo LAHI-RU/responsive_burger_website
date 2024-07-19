103eb9165fde1c3c724aa1aaefe8d63140da26ee
Lahiru Dhananjaya <lahiiru.dananjaya@gmail.com>
2024-07-20 01:00:03 +0530

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -19,7 +19,11 @@ if (navClose) {
 /*=============== REMOVE MENU MOBILE ===============*/
 const navLink = document.querySelectorAll(".nav__link");
 
-const linkAction = () => {};
+const linkAction = () => {
+  const navMenu = document.getElementById("nav-menu");
+  // When we click on each nav__link, we remove the show-menu class
+  navMenu.classList.remove("show-menu");
+};
 navLink.forEach((n) => n.addEventListener("click", linkAction));
 
 /*=============== CHANGE BACKGROUND HEADER ===============*/
